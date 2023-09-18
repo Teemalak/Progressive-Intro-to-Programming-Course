9bf34b8cd3b436fffbdc3d9f21238f38417237d6
Val Neculaescu <tinthetomato@gmail.com>
2023-09-18 15:58:31 -0400

    Ship the API!

---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/message", () =>
+
+{
+    var response = new MessageResponseModel("This is an API! Wow!", DateTime.Now);
+    return Results.Ok(response);
+});
+
+app.Run();
+
+
+public record MessageResponseModel(string Message, DateTime When);
